3aaaa366c295cf1ef8d08edab4479f24cf4553fd
jeevabalanmadhu <jeevabalan.m@fadv.com>
2022-07-16 09:38:19 +0530

    deploy heroku

--- doozywood/settings.py
+++ doozywood/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bkbz1w*3r_15$%zb$*p!pw(0jhe!-6&(9sv%whrw$ns%j+!m)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
